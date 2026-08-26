3289202293ecd70dbbfa415a0113b5ff7b9c1b0d
TA2ian <65396854+TA2ian@users.noreply.github.com>
2026-08-26 11:34:38 +0400

    test: align broadcast limit with branded template policy

--- tests/test_admin_broadcast_policy.py
+++ tests/test_admin_broadcast_policy.py
@@ -6,7 +6,7 @@ from handlers.admin_broadcast_policy import MAX_BROADCAST_LENGTH, is_admin
 
 class AdminBroadcastPolicyTests(unittest.TestCase):
     def test_broadcast_length_limit(self):
-        self.assertEqual(MAX_BROADCAST_LENGTH, 4096)
+        self.assertEqual(MAX_BROADCAST_LENGTH, 3500)
         self.assertEqual(len("x" * MAX_BROADCAST_LENGTH), MAX_BROADCAST_LENGTH)
         self.assertGreater(len("x" * (MAX_BROADCAST_LENGTH + 1)), MAX_BROADCAST_LENGTH)
 
